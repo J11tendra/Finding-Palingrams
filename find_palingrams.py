afb3a2c871dd2e208f42ae21946639a3fe042e0a
Jitendra Choudhary <108735984+J11tendra@users.noreply.github.com>
2023-05-16 13:00:48 +0530

    Update find_palingrams.py

--- find_palingrams.py
+++ find_palingrams.py
@@ -2,13 +2,13 @@
 from opening_file import open_file
 
 #Loading the list of words:
-word_list = open_file('D:/Python_work/text_files/All_Words.txt')
+word_list = open_file('Enter the text file')
 
 #Function to find word-pairs Palingrams:
 def find_palingrams():
 	'''find word-pairs palingrams and appending to list'''
 	palingrams_list = []
-	words = set(word_list)
+	words = set(word_list) #Converting it in set to optimize the code.
 	for word in words:
 		word_len = len(word)
 		reverse_word = word[::-1]
@@ -20,7 +20,12 @@ def find_palingrams():
 				    palingrams_list.append((reverse_word[:word_len-1], word))
 	return palingrams_list
 
+#Calling the Function:
 palingram = find_palingrams()
+
+#Sorting the list alphabetically:
 sorted_palingram = sorted(palingram)
 print(sorted_palingram)
+
+#How much palingram word-pairs we have found?:
 print(f'\n\n\nWe have found {len(sorted_palingram)} palingrams word-pairs in the wordlist.\n\n\n')
