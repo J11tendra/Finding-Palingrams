dd318e4ebe2c90f5a8d4120bae3e4bc2bd3e3d5d
Jitendra Choudhary <108735984+J11tendra@users.noreply.github.com>
2023-05-16 13:06:35 +0530

    Update cprofile_test.py

--- cprofile_test.py
+++ cprofile_test.py
@@ -1,3 +1,5 @@
-import cProfile
-import p
-cProfile.run('p.find_palingrams()')
+#Importing the cProfile library to get the statistics of the processing and the code file to be analyzed:
+import cProfile, find_palingrams
+
+#Calling the Function:
+cProfile.run('find_palingrams.find_palingrams()')
